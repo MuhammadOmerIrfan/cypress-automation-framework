d0e21af1c874bc003a80c26497c190e8c8b1aace
Omer <bscs18062@itu.edu.pk>
2024-02-06 12:37:44 +0500

    committs

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 const cucumber = require('cypress-cucumber-preprocessor').default;
 
 module.exports = defineConfig({
-  projectId: "oohp5e",
+  projectId: "4qkjxa",
   e2e: {
     setupNodeEvents(on, config) {
       on('file:preprocessor', cucumber())
