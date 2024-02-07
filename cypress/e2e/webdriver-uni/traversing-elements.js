ea8a88c11167e7f4129d7f645c8ca5e63f69160e
Omer <bscs18062@itu.edu.pk>
2024-02-07 17:37:32 +0500

    updating traversing-element

--- cypress/e2e/webdriver-uni/traversing-elements.js
+++ cypress/e2e/webdriver-uni/traversing-elements.js
@@ -8,28 +8,33 @@ describe("Traversing DOM elements in Cypress", () => {
     cy.get("#data-table").invoke("removeAttr", "target").click({ force: true });
   })
 
-  it.only("children() to get the children of DOM elements", () => {
+  it("children() to get the children of DOM elements", () => {
     cy.get(".traversal-breadcrumb").children('.active').should('contain','Contact Us')
   });
 
   it("closest() to validate the closest ancestor DOM element", () => {
-    
+    cy.get(".traversal-badge").closest('ul').should('have.class','list-group')
   });
 
   it("eq() to retrieve a specific element based on index", () => {
+    cy.get(".traversal-drinks-list > *").eq(2).should('contain','Milk')
+    
   });
 
   it("filter() to retrieve DOM elements that match a specific selector", () => {
-
+    cy.get(".btn-group-toggle >*").filter('.active').should('contain','Button-1')
   });
 
   it("find() to retrieve DOM elements of a given selector", () => {
+    cy.get(".traversal-pagination").find('li').find('a').should('have.length',7)
   });
 
   it("first() to retrieve the first DOM element within elements ", () => {
+    cy.get(".traversal-table > tbody > tr > td").first().should('contain','Andy')
   });
 
-  it("last() to retrieve the last DOM element within elements", () => {
+  it.only("last() to retrieve the last DOM element within elements", () => {
+    cy.get(".traversal-table > tbody > tr > td").last().should('contain','Scott')
   });
 
   it("nextAll() to get all of the next sibling DOM elements within elements", () => {
